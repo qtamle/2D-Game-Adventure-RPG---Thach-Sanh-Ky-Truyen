7f0bb28de7d45fea9dfa370054aecaf2bc47d79b
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-07-27 13:21:08 +0700

    update point quăng dây đu, fix lỗi leo thang

--- Assets/Script/Player/Grappler.cs
+++ Assets/Script/Player/Grappler.cs
@@ -1,40 +1,64 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
     public Camera mainCamera;
-
     public LineRenderer lineRenderer;
-
     public DistanceJoint2D distanceJoint;
+    public string pointTag = "Point";  // Tag cho các điểm grappler
+    public float maxGrappleDistance = 10f; // Khoảng cách tối đa để grappler
 
     private void Start()
     {
         distanceJoint.enabled = false;
+        lineRenderer.enabled = false;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            lineRenderer.SetPosition(0,mousePos);
-            lineRenderer.SetPosition(1, transform.position);
-            distanceJoint.connectedAnchor = mousePos;
-            distanceJoint.enabled = true;
-            lineRenderer.enabled = true;
+            Vector2 closestPoint = FindClosestPoint();
+            if (closestPoint != Vector2.zero)
+            {
+                lineRenderer.SetPosition(0, closestPoint);
+                lineRenderer.SetPosition(1, transform.position);
+                distanceJoint.connectedAnchor = closestPoint;
+                distanceJoint.enabled = true;
+                lineRenderer.enabled = true;
+            }
         }
-        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        else if (Input.GetKeyUp(KeyCode.E))
         {
             distanceJoint.enabled = false;
             lineRenderer.enabled = false;
         }
+
         if (distanceJoint.enabled)
         {
             lineRenderer.SetPosition(1, transform.position);
-
         }
+    }
+
+    private Vector2 FindClosestPoint()
+    {
+        GameObject[] points = GameObject.FindGameObjectsWithTag(pointTag);
+        float closestDistance = Mathf.Infinity;
+        Vector2 closestPoint = Vector2.zero;
+
+        foreach (GameObject point in points)
+        {
+            float distance = Vector2.Distance(transform.position, point.transform.position);
+            if (distance < closestDistance && distance <= maxGrappleDistance)
+            {
+                closestDistance = distance;
+                closestPoint = point.transform.position;
+            }
+        }
+
+        // Trả về Vector2.zero nếu không có điểm nào trong phạm vi
+        return closestDistance <= maxGrappleDistance ? closestPoint : Vector2.zero;
     }
 }
