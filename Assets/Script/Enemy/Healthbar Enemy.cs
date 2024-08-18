3a42068d6f2888cc88bf4d3e367fddb2ce25f8a2
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-08-18 20:35:48 +0700

    update color health bar

--- Assets/Script/Enemy/Healthbar Enemy.cs
+++ Assets/Script/Enemy/Healthbar Enemy.cs
@@ -7,14 +7,14 @@ public class HealthbarEnemy : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private Transform player;
     [SerializeField] private float detectionRadius = 5f;
-
     public float health;
     public float maxHealth = 100f;
-    public float smoothTime = 0.2f; // Thời gian để chuyển đổi mượt mà
+    public float smoothTime = 0.2f; 
 
     private float targetHealth;
     private float currentHealth;
     private float healthVelocity = 0f;
+    private Image fillImage;
 
     private KnockbackEnemy knockbackEnemy; 
 
@@ -31,6 +31,7 @@ public class HealthbarEnemy : MonoBehaviour
             slider.maxValue = maxHealth;
             slider.value = health;
             slider.gameObject.SetActive(false); // Ẩn thanh máu ban đầu
+            fillImage = slider.fillRect.GetComponent<Image>();
         }
     }
         
@@ -78,6 +79,7 @@ public class HealthbarEnemy : MonoBehaviour
             if (slider != null)
             {
                 slider.value = currentHealth;
+                UpdateHealthBarColor();
             }
             yield return null;
         }
@@ -90,6 +92,29 @@ public class HealthbarEnemy : MonoBehaviour
         if (targetHealth <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+    private void UpdateHealthBarColor()
+    {
+        if (targetHealth >= 80f)
+        {
+            fillImage.color = Color.green;
+        }
+        else if (targetHealth >= 60f && targetHealth < 80f)
+        {
+            fillImage.color = new Color(0.5f, 1f, 0.5f); 
+        }
+        else if (targetHealth >= 40f && targetHealth < 60f)
+        {
+            fillImage.color = Color.yellow;
+        }
+        else if (targetHealth >= 20f && targetHealth < 40f)
+        {
+            fillImage.color = new Color(1f, 0.64f, 0f); 
+        }
+        else
+        {
+            fillImage.color = Color.red;
         }
     }
 
--- Assets/Script/Player/Health Bar.cs
+++ Assets/Script/Player/Health Bar.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ public class HealthBar : MonoBehaviour
     public float maxHealth = 100f;
     public float health;
     public float lerpSpeed = 5f;
+    public Image fillImage;
 
     private float currentHealth;
 
@@ -16,7 +18,9 @@ public class HealthBar : MonoBehaviour
         health = maxHealth;
         currentHealth = health;
         healthSlider.maxValue = maxHealth; 
-        healthSlider.value = health; 
+        healthSlider.value = health;
+
+        fillImage = healthSlider.fillRect.GetComponent<Image>();
     }
 
     void Update()
@@ -24,14 +28,40 @@ public class HealthBar : MonoBehaviour
         currentHealth = Mathf.Lerp(currentHealth, health, Time.deltaTime * lerpSpeed);
         healthSlider.value = currentHealth;
 
-        if (health <= 0f)
+        UpdateHealthColor();
+
+        if (health < 0)
         {
             health = 0f;
+        }
+        else if (health > 100)
+        {
+            health = 100f;
         }
     }
 
     public void TakeDamage(float damage)
     {
         health -= damage;
+    }
+
+    private void UpdateHealthColor()
+    {
+        if (health >= 80f)
+        {
+            fillImage.color = Color.green;
+        }else if (health >= 60f && health < 80f)
+        {
+            fillImage.color = new Color(0.5f, 1f, 0.5f);
+        }else if (health >= 40f && health < 60f)
+        {
+            fillImage.color = Color.yellow;
+        }else if (health >= 20f && health < 40f)
+        {
+            fillImage.color = new Color(1f, 0.64f, 0f);
+        }else
+        {
+            fillImage.color = Color.red;
+        }
     }
 }
