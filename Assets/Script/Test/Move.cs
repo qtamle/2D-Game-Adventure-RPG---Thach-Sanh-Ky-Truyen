d8357ce9f34f45b9e81f6c0fdf943972530322e6
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-10-02 18:18:41 +0700

    test blend tree

---
+++ Assets/Script/Test/Move.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public float horizontal;
+    public bool moveRight;
+    private float jumpPower = 20f;
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    bool isGrounded = false;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+        }
+
+        if ((moveRight && horizontal > 0) || (!moveRight && horizontal < 0))
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
+
+    }
+
+    private void Flip()
+    {
+        moveRight = !moveRight; 
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1; 
+        transform.localScale = localScale;
+    }
+}
