38edee267c2bb1da0be5a232c7238cef380a4ee1
Tôm <lequoctamle55@gmail.com>
2024-12-03 18:44:26 +0700

    DCM

---
+++ Assets/TriggerTimelineTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTimelineTest : MonoBehaviour
+{
+    public GameObject active;
+    public Active activeTimeline;
+    private void Start()
+    {
+        active.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        active.SetActive(true);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            active.SetActive(false);
+                 activeTimeline.SetAndPlayTimeline(1);
+        }
+    }
+}
