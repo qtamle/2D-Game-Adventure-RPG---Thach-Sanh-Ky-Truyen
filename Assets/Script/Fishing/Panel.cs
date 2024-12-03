084ee7e484ac41358b0aea13abffaae1e7adc4eb
NicolasJone <146601910+NicolasJone@users.noreply.github.com>
2024-12-03 19:10:22 +0700

    Add UI Guide Fishing

---
+++ Assets/Script/Fishing/Panel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PanelToggle : MonoBehaviour
+{
+    public GameObject panel; 
+
+    void Start()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        // Kiểm tra nếu nhấn phím Tab
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (panel != null)
+            {
+                panel.SetActive(!panel.activeSelf);
+            }
+        }
+    }
+}
